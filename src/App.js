2a32dde72885f0189185b2d6f2f6803087409a86
Niv Ozer <niv.ozer@yahoo.com>
2024-01-29 03:22:00 +0200

    added starting State transition

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
 import State from './components/State'
 import Transition from './components/Transition'
-//https://github.com/Eliav2/react-xarrows/blob/master/README.md#path
-function App() {
 
-  const states = []
-  for (let i = 0; i < 5; i++){
-    const stateId = `q${i}`
-    states.push(<State key={i} id={stateId} accepting={true}/>)
-  }
+function App() {
+/*
+Automat = (Σ,Q,q0,F,δ)
+Σ - The Automat Alphabet
+Q - A finite list of states
+q0 - first state of the automat
+F - A list of the automat accepting states
+δ - Transitions Function δ:Σ X Q -> Q
+*/
+  // const states = []
+  // for (let i = 0; i < 5; i++){
+  //   const stateId = `q${i}`
+  //   states.push(<State key={i} id={stateId} accepting={true}/>)
+  // }
   return (
     <div className='Automat'>
-      {states}
-      <Transition fromState={"q1"} toState={"q4"}/>
-
+      <div id="startingpoint"></div>
+      <Transition fromState={"startingpoint"} toState={"q0"}/>
+      <State key={0} id={"q0"} accepting={false}/>
+      <Transition fromState={"q0"} toState={"q1"}/>
+      <State key={1} id={"q1"} accepting={true}/>
     </div>
   );
 }
--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,18 +1,25 @@
 import React from "react"
 import Xarrow from "react-xarrows";
+//https://github.com/Eliav2/react-xarrows/blob/master/README.md#path
 export default function Transition(props){
+    let transitionProps = {
+        start: props.fromState,
+        end: props.toState,
+        startAnchor: "right",
+        endAnchor: "left",
+        labels: <div className="transitionLabel">a</div>,
+        path: "smooth",
+        color: "black",
+        strokeWidth: 2.5,
+    };
+    if(props.fromState === "startingpoint"){
+        transitionProps.dashness = true
+        transitionProps.strokeWidth= 1.4
+        transitionProps.headSize=5
+    }
     return(
         <div>
-            <Xarrow
-                start={props.fromState}
-                end={props.toState}
-                startAnchor="right"
-                endAnchor="left"
-                labels= {<div className="transitionLabel">a</div>}
-                path="smooth"
-                color="black"
-                strokeWidth={2.5}
-            />
+            <Xarrow {...transitionProps}/>
         </div>
     )
 }
