a91eeb512ce7dd6f35ca0ee4ef1f99bd9dbdc288
Niv Ozer <niv.ozer@yahoo.com>
2024-02-27 03:12:30 +0200

    Added Banner and foundation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import State from "./components/State";
 import Transition from "./components/Transition";
 import Automat from "./components/Automat";
+import Header from "./components/Header";
 import React, { useState } from "react";
 
 //TODO: Add Transition positioning from an state to itself
-
+//TODO: Add input fields that create states
 function App() {
   return (
     <div>
+      <Header />
       <div className="AutomatPreview overflow w-full border border-gray-400 rounded-lg p-4 bg-white shadow-md">
-        <Automat className="w-full" />
+        <Automat />
       </div>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-center bg-slate-400 text-white font-bold py-4 px-6 rounded-lg">
+      Welcome to Automat Visualizer!
+    </div>
+  );
+}
+
+export default Header;
