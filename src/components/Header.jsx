f54b296b0fd337f61921613da57255c52eda3004
Niv Ozer <niv.ozer@yahoo.com>
2024-03-14 15:55:10 +0200

    WORKING

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="font-raleway text-xl flex flex-col items-center justify-center text-white font-bold pb-4">
       {/* <h4 className="m-0 p-1 mt-2">Welcome to Automat Visualizer!</h4> */}
       <img className="w-4/12 m-4" src={logo}></img>
-      <p className="text-purple-950 text-base mb-2 mt-0 p-1 font-medium text-lg">
+      <p className="text-purple-950 text-lg mb-2 mt-0 p-1 font-medium">
         A tool for visualizing DFA's and NFA's for Automats.
       </p>
       <AutomatConstructorToolbar />
