f5e32c8c9b9650543fe1906ba8effbd704c82bd6
Niv Ozer <niv.ozer@yahoo.com>
2024-03-17 02:07:01 +0200

    Fixed ToDos

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Automat from "./components/Automat";
 import Header from "./components/Header";
 import { React, useState } from "react";
 
-//TODO: Loops like(q2,q2,c) sometimes have label as q2 instead of c
-//TODO: States don't disappear after ""
 function App() {
   const [statesString, setStatesString] = useState("");
   const [transitionsString, setTransitionsString] = useState("");
