2203fb3060faa599a5fcfab4aca48eed0639276c
Niv Ozer <niv.ozer@yahoo.com>
2024-03-20 17:03:09 +0200

    Added prompt field

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import icon from "../images/AutomatVisualizerIconSVG.svg";
 import logo from "../images/AutomatVisualizerLogoSVG.svg";
 function Header({ onTransitionsChange, onStatesChange }) {
   return (
-    <div className="font-raleway text-xl flex flex-col items-center justify-center text-white font-bold pb-4">
+    <div className="font-raleway text-xl flex flex-col items-center justify-center text-white font-bold ">
       {/* <h4 className="m-0 p-1 mt-2">Welcome to Automat Visualizer!</h4> */}
       <img className="w-4/12 m-4" src={logo}></img>
       <p className="text-purple-950 text-lg mb-2 mt-0 p-1 font-medium">
--- src/components/PromptBaseCompletion.js
+++ src/components/PromptBaseCompletion.js
@@ -1,12 +1,18 @@
 import React from "react";
-import InputField from "./InputField";
 
 function PromptBaseCompletion() {
   return (
-    <div className="flex w-6/12 shadow-lg">
-      <div className="w-full ">
-        <InputField />
+    <div className="w-6/12">
+      <div className="full shadow-lg">
+        <input
+          className="text-center box-border text-xl shadow w-full border-none py-2 text-gray-700 border"
+          type="text"
+          placeholder="Specify an Automata"
+        ></input>
       </div>
+      <h6 className="p-1 m-0 flex justify-center text-zinc-300">
+        Utilizing ChatGPT
+      </h6>
     </div>
   );
 }
